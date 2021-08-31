b41842ffaea251661cf1e5fda78641859b657aed
Ayush783 <ayushc45xxx@gmail.com>
2021-08-31 20:14:23 +0530

    firebase auth service created

--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -1,1 +1,54 @@
-class FirebaseaAuthService {}
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseaAuthService {
+  static final _auth = FirebaseAuth.instance;
+
+  //sign in with email
+  Future<User> signInWithEmail(String email, String password) async {
+    try {
+      final userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential.user!;
+    } on FirebaseAuthException catch (e) {
+      //handle exception instead vof throwing exception
+      throw FirebaseAuthException(code: e.code);
+    }
+  }
+
+  //sign up with email
+  Future<User> signUpWithEmail(String email, String password) async {
+    try {
+      final userCredential = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential.user!;
+    } on FirebaseAuthException catch (e) {
+      //handle exception instead vof throwing exception
+      throw FirebaseAuthException(code: e.code);
+    }
+  }
+
+  //verify email
+  Future<void> sendVerifyEmailLink(User? user) async {
+    await user!.sendEmailVerification();
+  }
+
+  //check if email verified
+  bool checkEmailVerification(User? user) {
+    return user!.emailVerified ? true : false;
+  }
+
+  //sign out
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  //check if user is null
+  bool isUserNull() {
+    return _auth.currentUser == null ? true : false;
+  }
+
+  //auth state changes
+  Stream<User?> authStateChanges() async* {
+    yield* _auth.authStateChanges();
+  }
+}
