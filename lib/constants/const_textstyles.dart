c3fc864e0f6a000ba7305b94baaef721fbc98771
imutkarsh09 <itsutkarsh2711@gmail.com>
2021-09-02 14:01:25 +0530

    Necessary Changes made in SignUp screen

---
+++ lib/constants/const_textstyles.dart
@@ -0,0 +1,6 @@
+import "package:flutter/material.dart";
+
+TextStyle styleText(Color col, FontWeight wei, double size) {
+  return TextStyle(
+      color: col, fontWeight: wei, fontSize: size, fontFamily: 'IBM');
+}
