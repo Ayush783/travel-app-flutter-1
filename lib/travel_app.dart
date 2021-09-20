e6d0e49c8187bc028e18bacc1d8af80b14993e56
Ayush783 <ayushc45xxx@gmail.com>
2021-09-20 18:44:51 +0530

    auth bloc provider added

--- lib/travel_app.dart
+++ lib/travel_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/ui/auth/sign_in_screen.dart';
+import 'package:travel_app/ui/auth/auth_bloc_provider.dart';
 
 class TravelApp extends StatelessWidget {
   const TravelApp({Key? key}) : super(key: key);
@@ -10,7 +10,9 @@ class TravelApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Travel App',
       theme: ThemeData(primaryColor: Color(0xFF008665)),
-      home: SignInScreen(),
+      home: AuthBlocProvider(
+        screen: Screen.Signin,
+      ),
     );
   }
 }
